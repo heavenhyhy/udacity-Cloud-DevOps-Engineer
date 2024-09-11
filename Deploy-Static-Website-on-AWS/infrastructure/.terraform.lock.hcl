9b8d61409ed77c6bd82666b0a01d7cd90fdda713
sangtran <sang56pm@gmail.com>
2024-09-11 12:36:20 +0700

    init

---
+++ Deploy-Static-Website-on-AWS/infrastructure/.terraform.lock.hcl
@@ -0,0 +1,44 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "4.67.0"
+  constraints = "~> 4.16"
+  hashes = [
+    "h1:LfOuBkdYCzQhtiRvVIxdP/KGJODa3cRsKjn8xKCTbVY=",
+    "zh:0843017ecc24385f2b45f2c5fce79dc25b258e50d516877b3affee3bef34f060",
+    "zh:19876066cfa60de91834ec569a6448dab8c2518b8a71b5ca870b2444febddac6",
+    "zh:24995686b2ad88c1ffaa242e36eee791fc6070e6144f418048c4ce24d0ba5183",
+    "zh:4a002990b9f4d6d225d82cb2fb8805789ffef791999ee5d9cb1fef579aeff8f1",
+    "zh:559a2b5ace06b878c6de3ecf19b94fbae3512562f7a51e930674b16c2f606e29",
+    "zh:6a07da13b86b9753b95d4d8218f6dae874cf34699bca1470d6effbb4dee7f4b7",
+    "zh:768b3bfd126c3b77dc975c7c0e5db3207e4f9997cf41aa3385c63206242ba043",
+    "zh:7be5177e698d4b547083cc738b977742d70ed68487ce6f49ecd0c94dbf9d1362",
+    "zh:8b562a818915fb0d85959257095251a05c76f3467caa3ba95c583ba5fe043f9b",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9c385d03a958b54e2afd5279cd8c7cbdd2d6ca5c7d6a333e61092331f38af7cf",
+    "zh:b3ca45f2821a89af417787df8289cb4314b273d29555ad3b2a5ab98bb4816b3b",
+    "zh:da3c317f1db2469615ab40aa6baba63b5643bae7110ff855277a1fb9d8eb4f2c",
+    "zh:dc6430622a8dc5cdab359a8704aec81d3825ea1d305bbb3bbd032b1c6adfae0c",
+    "zh:fac0d2ddeadf9ec53da87922f666e1e73a603a611c57bcbc4b86ac2821619b1d",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.12.0"
+  hashes = [
+    "h1:S5xl3l6/rQmmujpKsHojuD1bPSo6F44JnlEWaRC03wU=",
+    "zh:019a4c09af254ef80b72cf0d843dfe72d99483e227138cf5b514a1b9977ab4c3",
+    "zh:0ae310ec740ebc6f275529507d60bb747d0bf39e72fc5a2fa90d74486006132c",
+    "zh:13d6aec117f05237fbf8c7d91d6ebb19797b00aa87e7a812642d3ea4738a394e",
+    "zh:2e87abbc261f9317d0c2ef26e01d5fabf77679da7d2cac6f47df7d198f720989",
+    "zh:4a6d471176ce0264455aa7d5457b8702f78400010c201c1719708958a1b7b647",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8978d5474a6da30bc0ad21c17db188d6918cacf3df3f6506b72ef3a268d53e2e",
+    "zh:b109efe138dfcb45dc04a9cc6809d185ab8b0ebc12040847c2dac430fda5af68",
+    "zh:b58e039b9106ac0a8de3c07f53b5279d7f0215fb35f2d23df642dfce0875382f",
+    "zh:ba2cbb2e515922d13efe3a46647be84f5426fcfcaa0f1520b3efeab8db847ed3",
+    "zh:c6c1ef1f26f25bca3abb5e07fa33dca37ed39cc26d0ff877964f2ffe5edd618c",
+    "zh:f8e171f923b7d2e789abd034072465dec3e6133c3a7644b7a7a965a74d52224e",
+  ]
+}
---
+++ Deploy-Static-Website-on-AWS/infrastructure/web.tf
@@ -0,0 +1,130 @@
+locals {
+  content_type_map = {
+    css         = "text/css"
+    html        = "text/html"
+    jpg         = "image/jpg"
+    js          = "text/javascript"
+    map         = "application/json"
+    json        = "application/json"
+    scss        = "text/x-scss"
+    svg         = "image/svg+xml"
+    eot         = "application/vnd.ms-fontobject"
+    ttf         = "font/ttf"
+    woff        = "font/woff"
+    woff2       = "font/woff2"
+  }
+  s3_origin_id      = "SangTD2 Static Website"
+}
+
+resource "aws_s3_bucket" "static_website" {
+  bucket = "81da30af-3fd5-44e4-b5b6-60110b443ba5-website-static-website" // 81da30af-3fd5-44e4-b5b6-60110b443ba5 my Udacity userId
+  force_destroy = true // delete all bucket objects when TF destroy
+}
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [aws_s3_bucket.static_website]
+
+  destroy_duration = "30s"
+}
+
+resource "aws_s3_bucket_policy" "static_website" {
+  depends_on = [time_sleep.wait_30_seconds] // to avoid 403, from my experience need to wait to create a bucket completely before updating policy
+
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "static_website"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.static_website.arn,
+          "${aws_s3_bucket.static_website.arn}/*",
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "upload_website" { // inheritance from Stackoverflow.
+  for_each      = fileset("../web/", "**/*.*")
+
+  bucket        = aws_s3_bucket.static_website.id
+  key           = each.value
+  source        = "../web/${each.value}"
+  content_type  = lookup(local.content_type_map, reverse(split(".", each.value))[0], "text/plain")
+  etag          = filemd5("../web/${each.value}") // no encryption so it's ok to use etag rather than source_hash, 
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "static_website" {
+  name                              = "static_website_origin_access"
+  description                       = "Static website origin access control"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "static_website_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_website.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.static_website.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  comment             = "Static website"
+  default_root_object = "index.html"
+
+  aliases = ["sangtd2.udacity.clouddevops.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
