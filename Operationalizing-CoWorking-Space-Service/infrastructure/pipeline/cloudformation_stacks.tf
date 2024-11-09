7d115a954192d69b033bbf8f1524468afffa8282
sangtran <sang56pm@gmail.com>
2024-11-09 13:33:31 +0700

    update pipeline

--- Operationalizing-CoWorking-Space-Service/infrastructure/pipeline/cloudformation_stacks.tf
+++ Operationalizing-CoWorking-Space-Service/infrastructure/pipeline/cloudformation_stacks.tf
@@ -6,7 +6,7 @@ resource "aws_cloudformation_stack" "app" {
   name = "analytics-application-pipeline"
   capabilities = ["CAPABILITY_IAM"]
   parameters = {
-    GitHubOAuthToken = data.aws_ssm_parameter.github_token
+    GitHubOAuthToken = data.aws_ssm_parameter.github_token.value
   }
   template_body = file("./cloudformation/pipeline.yml")
 }
