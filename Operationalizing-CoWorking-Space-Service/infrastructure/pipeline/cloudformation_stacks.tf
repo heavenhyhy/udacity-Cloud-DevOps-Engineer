a65d38c45c363e5dbffad5a3851f459f251e02b5
sangtran <sang56pm@gmail.com>
2024-11-11 12:51:32 +0700

    add Dockerfile

--- Operationalizing-CoWorking-Space-Service/infrastructure/pipeline/cloudformation_stacks.tf
+++ Operationalizing-CoWorking-Space-Service/infrastructure/pipeline/cloudformation_stacks.tf
@@ -3,10 +3,11 @@ data "aws_ssm_parameter" "github_token" {
 }
 
 resource "aws_cloudformation_stack" "app" {
+  depends_on = [ data.aws_ssm_parameter.github_token ]
   name = "analytics-application-pipeline"
   capabilities = ["CAPABILITY_IAM"]
   parameters = {
-    GitHubOAuthToken = data.aws_ssm_parameter.github_token.value
+    GitHubOAuthToken = "GitHubOAuthToken"
   }
   template_body = file("./cloudformation/pipeline.yml")
 }
